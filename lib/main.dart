e0743e67b1c74bcf65f187cb7c910eef57259665
htiketun <kohtiketun@outlook.com>
2025-02-14 16:54:18 +0000

    Update contact details; Add homepage URL; Refactor main app entry

--- lib/main.dart
+++ lib/main.dart
@@ -483,3 +483,4 @@ class MyPocketApp extends StatelessWidget {
 // Commit 40: 2025-02-13T12:46:12
 // Commit 41: 2025-02-13T19:51:48
 // Commit 43: 2025-02-14T09:46:01
+// Commit 44: 2025-02-14T16:54:18
