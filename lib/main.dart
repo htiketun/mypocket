606f2039c14dcd7459a640dd554c233548334fae
htiketun <kohtiketun@outlook.com>
2025-02-16 11:02:38 +0000

    Improve widget tree structure

--- lib/main.dart
+++ lib/main.dart
@@ -485,3 +485,4 @@ class MyPocketApp extends StatelessWidget {
 // Commit 43: 2025-02-14T09:46:01
 // Commit 44: 2025-02-14T16:54:18
 // Commit 45: 2025-02-14T23:41:00
+// Commit 50: 2025-02-16T11:02:38
