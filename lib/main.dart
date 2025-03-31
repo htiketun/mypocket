ac5c18044cee507b833c9392a50994251d96e2a7
htiketun <kohtiketun@outlook.com>
2025-03-31 06:30:26 +0000

    Refactor project metadata; Update navigation logic

--- lib/main.dart
+++ lib/main.dart
@@ -559,3 +559,4 @@ class MyPocketApp extends StatelessWidget {
 // Commit 187: 2025-03-28T20:54:34
 // Commit 190: 2025-03-29T19:04:51
 // Commit 193: 2025-03-30T16:07:49
+// Commit 195: 2025-03-31T06:30:26
